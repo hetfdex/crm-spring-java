c9971a4f56993745414ab43468429d7865f35da8
Jose Pereira <hetfdex@gmail.com>
2019-01-08 16:58:14 +0000

    Customer controller implemented

--- src/com/hetfdex/crm/controller/CustomerController.java
+++ src/com/hetfdex/crm/controller/CustomerController.java
@@ -1,15 +1,28 @@
 package com.hetfdex.crm.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.hetfdex.crm.dao.CustomerDAO;
+import com.hetfdex.crm.entity.Customer;
+
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+	@Autowired
+	CustomerDAO customerDAO;
+
 	@GetMapping("/list")
 	public String getCustomers(Model model) {
+		List<Customer> customers = customerDAO.getCustomers();
+
+		model.addAttribute("customers", customers);
+
 		return "list-customers";
 	}
 }
--- src/com/hetfdex/crm/dao/impl/CustomerDAOImpl.java
+++ src/com/hetfdex/crm/dao/impl/CustomerDAOImpl.java
@@ -8,10 +8,12 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.hetfdex.crm.dao.CustomerDAO;
 import com.hetfdex.crm.entity.Customer;
 
+@Repository
 public class CustomerDAOImpl implements CustomerDAO {
 	@Autowired
 	private SessionFactory sessionFactory;
@@ -20,9 +22,9 @@ public class CustomerDAOImpl implements CustomerDAO {
 	@Transactional
 	public List<Customer> getCustomers() {
 		Session session = sessionFactory.getCurrentSession();
-		
+
 		Query<Customer> query = session.createQuery("from Customer", Customer.class);
-		
+
 		return query.getResultList();
 	}
 }
